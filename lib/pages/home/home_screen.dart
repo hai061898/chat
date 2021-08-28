83eaaf077292a1f554816f127b0bbbb9a1f04215
hai061898 <truonghaiauto98@gmail.com>
2021-08-28 15:20:53 +0700

    #2 dropdownlist

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat/pages/chat/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -18,35 +19,59 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  
-      appBar: AppBar(
-        title: Text("Chat"),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.more_vert)),
-        ],
-        bottom:  TabBar(  
-          controller: controller,
-          tabs: [
-            Tab(
-              icon: Icon(Icons.camera_alt),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Chat"),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            PopupMenuButton<String>(
+              onSelected: (value) {
+                print(value);
+              },
+              itemBuilder: (BuildContext context) {
+                return [
+                  PopupMenuItem(
+                    child: Text("New Group"),
+                    value: "New group",
+                  ),
+                  PopupMenuItem(
+                    child: Text("Message"),
+                    value: "message",
+                  ),
+                  PopupMenuItem(
+                    child: Text("Setting"),
+                    value: "setting",
+                  ),
+                ];
+              },
             ),
-            Tab(  
-              text: "Chat",
-            ),
-             Tab(  
-              text: "Chat",
-            ),
-             Tab(  
-              text: "Chat",
-            ),
-
           ],
+          bottom: TabBar(
+            controller: controller,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.camera_alt),
+              ),
+              Tab(
+                text: "Chat",
+              ),
+              Tab(
+                text: "Group",
+              ),
+              Tab(
+                text: "Call",
+              ),
+            ],
+          ),
         ),
-      ),
-      body: Container(
-        child: Text("aaa"),
-      ),
-    );
+        body: TabBarView(
+          controller: controller,
+          children: [
+            Text("1"),
+            ChatScreen(),
+            Text("3"),
+            Text("4"),
+          ],
+        ));
   }
 }
